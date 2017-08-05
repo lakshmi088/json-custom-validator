82e8db98fb49171c510477448b49cfc5c6176838
swaroop <d708517e-0a4e-40f2-ae6b-b5d824f763b4@localhost>
2017-08-05 21:18:06 +0530

    init commit with resources

---
+++ src/main/java/com/jsonpoc/service/JSONService.java
@@ -0,0 +1,24 @@
+package com.jsonpoc.service;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.networknt.schema.JsonSchema;
+import com.networknt.schema.JsonSchemaFactory;
+import java.io.File;
+import java.io.FileInputStream;
+import java.net.URL;
+
+public class JSONService {
+
+    public JsonNode getJsonNodeFromFile(String path) throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode node = mapper.readTree(new FileInputStream(new File(path)));
+        return node;
+    }
+
+    public JsonSchema getJsonSchemaFromFile(String path) throws Exception {
+        JsonSchemaFactory factory = new JsonSchemaFactory();
+        JsonSchema schema = factory.getSchema(new FileInputStream(new File(path)));
+        return schema;
+    }
+}
